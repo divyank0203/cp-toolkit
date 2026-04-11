f342a384450aa014ab4a8d3c28260e79ca63d543
divyank0203 <divyankh444@gmail.com>
2026-04-11 23:10:14 +0530

    cors origin update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(morgan('dev'));
